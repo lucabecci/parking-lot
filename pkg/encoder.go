e57d859d9a9705ea02f709c43153e0a5923457bc
luca <beccibrian@gmail.com>
2021-03-16 12:47:56 -0300

    chore(Pkg ): Encoder added for all services

---
+++ pkg/encoder.go
@@ -0,0 +1,11 @@
+package pkg
+
+import (
+	"context"
+	"encoding/json"
+	"net/http"
+)
+
+func EncodeResponse(_ context.Context, w http.ResponseWriter, response interface{}) error {
+	return json.NewEncoder(w).Encode(response)
+}
