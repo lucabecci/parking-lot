2fa68094dc2a052ef20bef5efee572ade0c46392
luca <beccibrian@gmail.com>
2021-03-16 14:49:03 -0300

    chore(Pkg): User repository added

--- pkg/error.go
+++ pkg/error.go
@@ -1,9 +1,11 @@
-package errors
+package pkg
 
 import "errors"
 
 var (
 	ErrInvalidPassword = errors.New("Invalid Password")
 	ErrEmailInUse      = errors.New("Email already in use")
+	ErrEmailNotExists  = errors.New("Email not registered")
 	ErrInvalidToken    = errors.New("Token invalid")
+	ErrToCreate        = errors.New("Error to create your user")
 )
--- pkg/repository/user_repository.go
+++ pkg/repository/user_repository.go
@@ -1,1 +1,47 @@
 package repository
+
+import (
+	"github.com/lucabecci/parking-lot/pkg"
+	"github.com/lucabecci/parking-lot/pkg/models"
+	"gorm.io/gorm"
+)
+
+type UserRepository struct {
+	database *gorm.DB
+}
+
+func (ur *UserRepository) Create(email, password string) (models.User, error) {
+	user := models.User{
+		Email:    email,
+		Password: password,
+	}
+
+	user.HashPassword()
+	result := ur.database.Create(&user)
+
+	if result.RowsAffected < 1 {
+		return models.User{}, pkg.ErrToCreate
+	}
+
+	result.Scan(&user)
+
+	return user, nil
+}
+
+func (ur *UserRepository) GetByEmail(email string) (models.User, error) {
+	var usr models.User
+	result := ur.database.Where("email = ?", email).Find(&usr)
+	if result.RowsAffected < 1 {
+		return models.User{}, pkg.ErrEmailNotExists
+	}
+	return usr, nil
+}
+
+func (ur *UserRepository) GetByID(id uint) (models.User, error) {
+	var usr models.User
+	result := ur.database.Where("id = ?", id).Find(&usr)
+	if result.RowsAffected < 1 {
+		return models.User{}, pkg.ErrEmailNotExists
+	}
+	return usr, nil
+}
