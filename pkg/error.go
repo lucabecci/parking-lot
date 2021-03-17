c0ac2ca5199151862fe50d1ab3efe9be8b3f99d1
luca <beccibrian@gmail.com>
2021-03-16 23:30:37 -0300

    chore(Auth): More functionality added in the auth service

--- pkg/error.go
+++ pkg/error.go
@@ -3,9 +3,10 @@ package pkg
 import "errors"
 
 var (
-	ErrInvalidPassword = errors.New("Invalid Password")
-	ErrEmailInUse      = errors.New("Email already in use")
-	ErrEmailNotExists  = errors.New("Email not registered")
-	ErrInvalidToken    = errors.New("Token invalid")
-	ErrToCreate        = errors.New("Error to create your user")
+	ErrInvalidPassword  = errors.New("Invalid Password")
+	ErrEmailInUse       = errors.New("Email already in use")
+	ErrEmailNotExists   = errors.New("Email not registered")
+	ErrInvalidToken     = errors.New("Token invalid")
+	ErrToCreate         = errors.New("Error to create your user")
+	ErrPasswordNotEqual = errors.New("Password not equal")
 )
--- services/auth/service/service.go
+++ services/auth/service/service.go
@@ -1,15 +1,50 @@
 package service
 
-import "context"
+import (
+	"context"
+
+	"github.com/lucabecci/parking-lot/pkg"
+	"github.com/lucabecci/parking-lot/pkg/models"
+	"github.com/lucabecci/parking-lot/pkg/repository"
+)
 
 type Service interface {
 	Login(ctx context.Context, email, password string) (string, error)
-	Register(ctx context.Context, email, password, confirmPassword string) error
+	Register(ctx context.Context, email, password, confirmPassword string) (models.User, error)
 	ValidateToken(ctx context.Context, token string) error
 }
 
-type service struct{}
+type service struct {
+	respository repository.UserRepository
+}
 
 func GetService() *service {
 	return &service{}
+}
+
+func (s *service) Register(ctx context.Context, email, password, confirmPassword string) (models.User, error) {
+	if password != confirmPassword {
+		return models.User{}, pkg.ErrPasswordNotEqual
+	}
+	usr, err := s.respository.Create(email, password)
+	if err != nil {
+		return models.User{}, pkg.ErrToCreate
+	}
+	return usr, nil
+}
+
+func (s *service) Login(ctx context.Context, email, password string) (string, error) {
+	user, err := s.respository.GetByEmail(email)
+	if err != nil {
+		return "", err
+	}
+	match := user.PasswordMatch(password)
+	if match == false {
+		return "", pkg.ErrInvalidPassword
+	}
+	return "", nil
+}
+
+func (s *service) ValidateToken(ctx context.Context, token string) error {
+	return nil
 }
